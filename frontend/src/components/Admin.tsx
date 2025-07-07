b8149b2691dc3202774a3be952eac56f858af1dd
Michael Mongelli <mmongelli99@gmail.com>
2025-07-07 08:00:21 -0400

    populate admin page droppable fields and detect dragging and dropping

--- frontend/src/components/Admin.tsx
+++ frontend/src/components/Admin.tsx
@@ -1,28 +1,29 @@
 import React, { useState, useEffect } from "react";
-import {
-  DndContext,
-  useDraggable,
-  useDroppable,
-  DragEndEvent,
-} from "@dnd-kit/core";
+import { Draggable, Droppable } from "./DragAndDrop";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
 import { getWizardComponents, updateWizardComponent } from "../services";
 
+// const DraggableComponent = ();
+
 export default function Admin() {
-  type WizardSteps = Record<
-    number, // wizard step number
+  type WizardStepsConfig = Record<
+    string, // wizard step number
     string[] // components present in step of wizard
   >;
-  const wizardStepsInit = {
-    0: [] as string[], // unused components
+  const unusedComponentsKey = 0;
+  const wizardStepsConfigInit = {
+    [unusedComponentsKey]: [] as string[], // unused components
   };
-  const [wizardSteps, setWizardSteps] = useState<WizardSteps>(wizardStepsInit);
+  const [wizardStepsConfig, setWizardStepsConfig] = useState<WizardStepsConfig>(
+    wizardStepsConfigInit,
+  );
 
   // get wizard steps configuration from db
   useEffect(() => {
     function handleSuccess(data: unknown) {
-      setWizardSteps(
+      setWizardStepsConfig(
         (data as { kind: string; step: number }[]).reduce(
-          (acc: WizardSteps, { kind, step }) => {
+          (acc: WizardStepsConfig, { kind, step }) => {
             const componentName = kind;
             const wizardStepNumber = step;
             if (!acc[wizardStepNumber]) acc[wizardStepNumber] = [];
@@ -31,7 +32,7 @@ export default function Admin() {
             acc[wizardStepNumber] = [...new Set(acc[wizardStepNumber])];
             return acc;
           },
-          wizardStepsInit,
+          wizardStepsConfigInit,
         ),
       );
     }
@@ -42,6 +43,27 @@ export default function Admin() {
       },
     });
   }, []);
+
+  function handleDragEnd(event: DragEndEvent) {
+    const {
+      active, // active item being dragged
+      over, // area item has been dropped over
+    } = event;
+    if (!over || !active) return;
+
+    // wizard step numbers
+    const fromStep = active.id;
+    const toStep = over.id;
+
+    console.log("from", fromStep, "to", toStep);
+
+    if (fromStep === toStep) return;
+
+    // setWizardSteps((prev) => {
+    //   const curr = prev;
+    //
+    // });
+  }
 
   return (
     <div className="p-8 min-h-screen text-white bg-gray-900">
@@ -61,7 +83,58 @@ export default function Admin() {
           • Each step of the wizard must have one component.
         </li>
       </ul>
-      <p>{JSON.stringify(wizardSteps)}</p>
+      <DndContext onDragEnd={handleDragEnd}>
+        <div className="flex flex-row space-x-12">
+          <div className="w-1/2">
+            <h3 className="text-2xl mb-2">Wizard Components:</h3>
+            <div className="flex flex-col space-y-6">
+              <Droppable id={String(unusedComponentsKey)}>
+                {[
+                  ...wizardStepsConfig[unusedComponentsKey].map(
+                    (componentName) => (
+                      <Draggable
+                        displayName={componentName}
+                        key={componentName}
+                        id={componentName}
+                      ></Draggable>
+                    ),
+                  ),
+                ]}
+              </Droppable>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <h3 className="text-2xl mb-2">Wizard Steps:</h3>
+            <div className="flex flex-col space-y-6">
+              {Object.keys(wizardStepsConfig).map((wizardStepNumber) => {
+                if (Number(wizardStepNumber) === unusedComponentsKey) return;
+                return (
+                  <Droppable
+                    displayName={`Step ${wizardStepNumber}`}
+                    key={wizardStepNumber}
+                    id={wizardStepNumber}
+                  >
+                    {[...wizardStepsConfig[wizardStepNumber]].map(
+                      (componentName) => {
+                        const isOnlyComponentInWizardStep =
+                          wizardStepsConfig[wizardStepNumber].length === 1;
+                        return (
+                          <Draggable
+                            displayName={componentName}
+                            key={componentName}
+                            id={componentName}
+                            disabled={isOnlyComponentInWizardStep}
+                          ></Draggable>
+                        );
+                      },
+                    )}
+                  </Droppable>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </DndContext>
     </div>
   );
 }
---
+++ frontend/src/components/DragAndDrop.tsx
@@ -0,0 +1,64 @@
+import {
+  DndContext,
+  useDraggable,
+  useDroppable,
+  DragEndEvent,
+} from "@dnd-kit/core";
+
+export function Draggable({
+  displayName,
+  id,
+  disabled = false,
+}: {
+  displayName?: string;
+  id: string;
+  disabled?: boolean;
+}) {
+  // an item which can be dragged into a Droppable container
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id,
+    disabled,
+  });
+  const style = {
+    transform: transform
+      ? `translate(${transform.x}px, ${transform.y}px)`
+      : undefined,
+    opacity: disabled ? 0.5 : 1,
+    cursor: disabled ? "not-allowed" : "move",
+  };
+  return (
+    <div
+      ref={setNodeRef}
+      {...(!disabled ? listeners : {})}
+      {...attributes}
+      style={style}
+      className="p-2 m-2 bg-blue-500 text-white rounded"
+    >
+      {displayName ?? ""}
+    </div>
+  );
+}
+
+export function Droppable({
+  displayName,
+  id,
+  children,
+}: {
+  displayName?: string;
+  id: string;
+  children?: React.ReactNode;
+}) {
+  // a container into which draggable items can be dropped
+  const { isOver, setNodeRef } = useDroppable({ id });
+  const bgColor = isOver ? "bg-green-300" : "bg-gray-500";
+
+  return (
+    <div
+      ref={setNodeRef}
+      className={`min-h-[100px] p-4 m-2 rounded border border-dashed border-gray-500 ${bgColor}`}
+    >
+      <p className="font-semibold mb-2">{displayName ?? ""}</p>
+      {children}
+    </div>
+  );
+}
